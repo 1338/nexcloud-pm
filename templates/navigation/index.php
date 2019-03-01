f96d281b4ea503a409c2d42c55b0152030e6debb
1338 <maxvandersluis@yahoo.com>
2019-03-01 11:47:27 +0100

    INIT

---
+++ templates/navigation/index.php
@@ -0,0 +1,10 @@
+<ul>
+	<li><a href="#">Project ProjectManagement</a></li>
+	<li>
+		<a href="#">First level container</a>
+		<ul>
+			<li><a href="#">Second level entry</a></li>
+			<li><a href="#">Second level entry</a></li>
+		</ul>
+	</li>
+</ul>
